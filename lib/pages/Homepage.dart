e9617a1fa674733a5b85cd434d98d29adbe95afd
kmsaiful <kmsaifulislam123@gmail.com>
2022-10-05 22:13:06 +0600

    added Items Widget

--- lib/pages/Homepage.dart
+++ lib/pages/Homepage.dart
@@ -1,6 +1,9 @@
 import 'package:ecommerce_one/widgets/HomeAppBar.dart';
 import 'package:flutter/material.dart';
 
+import '../widgets/CategoriesWidget.dart';
+import '../widgets/ItemsWidget.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -8,7 +11,85 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(children: [
-        HomeAppBar(),
+        const HomeAppBar(),
+        Container(
+          // height: 500,
+          padding: const EdgeInsets.only(top: 15),
+          decoration: const BoxDecoration(
+            color: Color(0xFFEDECF2),
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(35),
+              topRight: Radius.circular(35),
+            ),
+          ),
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 15),
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(left: 5),
+                      height: 50,
+                      width: 300,
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Search here..."
+                        ),
+                      ),
+                    ),
+                    const Spacer(),
+                    const Icon(
+                      Icons.camera_alt,
+                      size: 27,
+                      color: Color(0xFF4C53A5),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                margin: const EdgeInsets.symmetric(
+                  vertical: 20,
+                  horizontal: 10,
+                ),
+                child: const Text(
+                  "Categories",
+                  style: TextStyle(fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF4C53A5),
+                  ),
+                ),
+              ),
+
+              CategoriesWidget(),
+
+              //Items
+              Container(
+                alignment: Alignment.centerLeft,
+                margin: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                child: Text(
+                  "Best Selling",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF4C53A5),
+                  ),
+                ),
+              ),
+
+              // Items Widget
+              ItemsWidget(),
+            ],
+          ),
+        ),
       ],),
     );
   }
--- lib/widgets/HomeAppBar.dart
+++ lib/widgets/HomeAppBar.dart
@@ -31,11 +31,14 @@ class HomeAppBar extends StatelessWidget {
           Badge(
             badgeColor: Colors.red,
             padding: EdgeInsets.all(7),
-            badgeContent: Text("3"),
+            badgeContent: Text("3", style: TextStyle(color: Colors.white),
+            ),
             child: InkWell(
               onTap: (){},
               child: Icon(
                 Icons.shopping_bag_outlined,
+                size: 30,
+                color: Color(0xFF4C53A5),
               ),
             ),
           ),
